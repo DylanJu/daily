9ab92fc97bad7a0cd3412563e1bc88db3dbad893
DylanJu <benefit.joo@gmail.com>
2023-10-29 23:42:28 +0900

    feat: add basic route pages

--- src/routes/layout.tsx
+++ src/routes/layout.tsx
@@ -2,6 +2,8 @@ import { component$, Slot } from "@builder.io/qwik";
 import { routeLoader$ } from "@builder.io/qwik-city";
 import type { RequestHandler } from "@builder.io/qwik-city";
 
+import AppHeader from "~/components/app-header/app-header";
+
 export const onGet: RequestHandler = async ({ cacheControl }) => {
   // Control caching for this request for best performance and to reduce hosting costs:
   // https://qwik.builder.io/docs/caching/
@@ -22,6 +24,7 @@ export const useServerTimeLoader = routeLoader$(() => {
 export default component$(() => {
   return (
     <main class="container">
+      <AppHeader />
       <Slot />
     </main>
   );
--- src/routes/organize/index.tsx
+++ src/routes/organize/index.tsx
@@ -1,21 +1,14 @@
 import { component$ } from "@builder.io/qwik";
 import type { DocumentHead } from "@builder.io/qwik-city";
 
-
 export default component$(() => {
   return (
     <div>
-      <h1>Real-me</h1>
+      <h1>Organize</h1>
     </div>
   );
 });
 
 export const head: DocumentHead = {
-  title: "Real-me",
-  meta: [
-    {
-      name: "description",
-      content: "Qwik site description",
-    },
-  ],
+  title: "Organize",
 };
