5aef989da27918e1c338eeada371599939bdd5d3
DylanJu <benefit.joo@gmail.com>
2023-10-29 23:22:38 +0900

    feat: add hambuerger-button css

--- src/components/app-header/hamburger-menu/hamburger-menu.tsx
+++ src/components/app-header/hamburger-menu/hamburger-menu.tsx
@@ -1,9 +1,19 @@
 import { component$ } from "@builder.io/qwik";
 
+import styles from "./hamburger-menu.module.css";
+
 export default component$(() => {
   return (
-    <button type="button">
-      H
+    <button
+      class={[styles.hamburger, styles["hamburger--spring"]]}
+      type="button"
+      onClick$={(event, element) => {
+        element.classList.toggle(styles["is-active"]);
+      }}
+    >
+      <span class={styles["hamburger-box"]}>
+        <span class={styles["hamburger-inner"]}></span>
+      </span>
     </button>
   );
 });
