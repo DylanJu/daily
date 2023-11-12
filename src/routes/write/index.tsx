f0893c545af47f3359f6dfca2362dbe08b63d32a
DylanJu <benefit.joo@gmail.com>
2023-11-12 15:26:02 +0900

    이것저것 테스트
    1. routeActions$
    qwik에서 폼 액션을 다루는 방법. onPost, onGet, onRequest 등과 내부적으로 크게 다르지 않음..?
    
    2. onPost
    마지막으로 시도해본것. next로 넘기는 방식을 제외하면 라우트액션과 비슷..?
    아니겠다. 온클릭과 url http protocol을 보고 리턴해주는거 자체가 다를수도? 테스트 좀 더 필요.
    단 여기서 env로 cf d1 접근가능한 단서 발견.
    dev 환경 개발을 위해 미니플레이어 필요

--- src/routes/write/index.tsx
+++ src/routes/write/index.tsx
@@ -1,10 +1,37 @@
 import { component$ } from "@builder.io/qwik";
-import type { DocumentHead } from "@builder.io/qwik-city";
+import { routeAction$, Form } from "@builder.io/qwik-city";
+import type { DocumentHead, RequestHandler } from "@builder.io/qwik-city";
+
+export const useAddJourney = routeAction$(async (data, context) => {
+  console.log(context.env);
+
+  return { success: true, content: "123" };
+})
+
+export const onPost: RequestHandler = async ({next, env, platform}) => {
+  console.log('env', env);
+  console.log('platform', Object.keys(platform));
+  console.log('platform.env', platform.env?.['DB']);
+  // console.log('Before request', url);
+  await next();
+  // console.log('After request', url);
+};
 
 export default component$(() => {
+  const action = useAddJourney();
+
   return (
     <div>
       <h1>Write</h1>
+      <Form action={action}>
+        <textarea name="content" />
+        <button type="submit">submit</button>
+      </Form>
+
+      {action.value?.success && (
+        // When the action is done successfully, the `action.value` property will contain the return value of the action
+        <p>User {action.value.content} added successfully</p>
+      )}
     </div>
   );
 });
