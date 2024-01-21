b748ecb5a320fb76fa53e1cd3ca93d718cc03144
DylanJu <benefit.joo@gmail.com>
2024-01-22 07:45:35 +0900

    feat: 기본 addTodo 구현
    uuid 추가되는 기본기능만 구현한 상태
    remove와 update는 동작안됨
    ui 정리안함

--- src/components/todo/Todo.tsx
+++ src/components/todo/Todo.tsx
@@ -1,18 +1,63 @@
-// import type { QRL, QwikKeyboardEvent } from "@builder.io/qwik";
-import { component$, Slot } from "@builder.io/qwik";
+import type { QRL } from "@builder.io/qwik";
+import { component$, $, useVisibleTask$, useSignal } from "@builder.io/qwik";
 
-export const Todo = component$<{
+import type { Todo } from "~/routes/organize";
+
+const TodoRow = component$<{
   id: string;
-  // onKeyDown$: QRL<
-  //   (
-  //     e: QwikKeyboardEvent<HTMLDivElement>,
-  //     currentTarget: HTMLDivElement
-  //   ) => void
-  // >;
-}>(({ id }) => {
+  todo: Todo;
+  addTodo: QRL<() => void>;
+  removeTodo: QRL<(uuid: string) => void>;
+  updateTodo: QRL<(uuid: string, todo: Todo) => void>;
+}>(({ id, todo: { checked }, addTodo, removeTodo, updateTodo }) => {
+  const contentEditableRef = useSignal<HTMLElement>();
+
+  const onKeyDown$ = $((e: KeyboardEvent) => {
+    console.group("key down");
+    console.log(e.key);
+    console.log(contentEditableRef.value?.textContent);
+    console.groupEnd();
+
+    if (e.key === "Enter") {
+      addTodo();
+      e.preventDefault();
+      return;
+    }
+
+    if (e.key === "Delete" && !contentEditableRef.value!.textContent) {
+      removeTodo(id);
+      return;
+    }
+
+    updateTodo(id, {
+      checked,
+      text: contentEditableRef.value?.textContent || "",
+    });
+  });
+
+  useVisibleTask$(({ cleanup }) => {
+    contentEditableRef.value!.addEventListener("keydown", onKeyDown$);
+
+    cleanup(() => {
+      contentEditableRef.value!.removeEventListener("keydown", onKeyDown$);
+    });
+  });
+
   return (
-    <div id={id}>
-      <Slot />
+    <div data-id={id}>
+      <input type="checkbox" checked={checked} />
+      <div
+        id={id}
+        ref={contentEditableRef}
+        contentEditable="true"
+        // onKeyDown$={(e) => {
+        //   console.log("key down", e.key);
+        // }}
+        // preventdefault:keydown
+      >
+      </div>
     </div>
   );
 });
+
+export default TodoRow;
--- src/routes/organize/index.tsx
+++ src/routes/organize/index.tsx
@@ -1,59 +1,60 @@
-import { component$, $, useSignal, useTask$ } from "@builder.io/qwik";
-import type { QwikKeyboardEvent } from "@builder.io/qwik";
+import {
+  component$,
+  useSignal,
+  useTask$,
+  $,
+} from "@builder.io/qwik";
 import type { DocumentHead } from "@builder.io/qwik-city";
 import { v4 as uuid } from "uuid";
 
-import { Todo } from "~/components/todo/Todo";
+import TodoRow from "~/components/todo/Todo";
 import styles from "./index.module.css";
 
+export type Todo = {
+  checked: boolean;
+  text: string;
+};
+
 export default component$(() => {
-  const uuids = useSignal<Map<string, string>>(new Map());
-  // uuid 배열관리
-  // 엔터키 누르면, 현재 커서 다음에 새로운 uuid 추가하고 TODO 컴포넌트 생성, 커서이동 -> onKeyDown$ 이벤트로 처리
-  // delete키 누르면, 현재 커서의 uuid 삭제하고 TODO 컴포넌트 삭제, 커서이동 -> onKeyDown$ 이벤트로 처리
-  // onKeyDown$ 상속시키기
-
-  const onKeyDown$ = $(
-    (
-      event: QwikKeyboardEvent<HTMLDivElement>,
-      currentTarget: HTMLDivElement
-    ) => {
-      console.log("target", event.target);
-      console.log("currentTarget", currentTarget);
-
-      // if (event.key === "Enter") {
-      // }
-    }
-  );
+  const todos = useSignal<Map<string, Todo>>(new Map());
 
   const addTodo = $(() => {
     const newId = uuid();
 
-    if (uuids.value.has(newId)) {
+    if (todos.value.has(newId)) {
       // addTodo();
       return;
     }
+    const newTodos = new Map(todos.value);
+    newTodos.set(newId, { checked: false, text: "" });
+    todos.value = newTodos;
+  });
 
-    uuids.value.set(newId, "");
+  const removeTodo = $((uuid: string) => {
+    todos.value.delete(uuid);
+  });
+
+  const updateTodo = $((uuid: string, todo: Todo) => {
+    todos.value.set(uuid, todo);
   });
 
   useTask$(() => {
-    addTodo();
+    todos.value.set(uuid(), { checked: false, text: "" });
   });
 
   return (
     <>
-      <div>{Array.from(uuids.value).map(([k]) => k + "\n")}</div>
-      <div
-        contentEditable="true"
-        class={styles.editor}
-        preventdefault:keydown
-        onKeyDown$={onKeyDown$}
-      >
-        {Array.from(uuids.value).map(([uuid, text]) => (
-          <Todo key={uuid} id={uuid}>
-            {text}
-          </Todo>
+      <div>{Array.from(todos.value).map(([k]) => k + "\n")}</div>
+      <div id="editor" class={styles.editor}>
+        {Array.from(todos.value).map(([uuid, todo]) => (
+          <TodoRow
+            key={uuid}
+            id={uuid}
+            todo={todo}
+            addTodo={addTodo}
+            removeTodo={removeTodo}
+            updateTodo={updateTodo}
+          />
         ))}
       </div>
     </>
